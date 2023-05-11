9cbbc2ea2f6f78385ec98d5a6973a6e208de1c9b
sheetal216 <96660146+sheetal216@users.noreply.github.com>
2023-05-11 09:34:49 +0530

    Delete Main directory

--- Main/static/JS Files/DriverLogin.js
+++
@@ -1,9 +0,0 @@
-function formSubmitted()
-{
-    var e = document.getElementById("emailinput").value;
-    var p = document.getElementById("passwordinput").value;
-    if (e.toLowerCase()=="driver@coldmail.com" && p=="d123")
-        window.location.href = "driver_home.html";
-    else
-        window.alert("Incorrect Email Id or Password");
-}
